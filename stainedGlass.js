1fe14e12fd2b74f1cf557ef53a913cf8ab6fb35a
Joshua Sims <JoshuaSimsVA@gmail.com>
2017-02-16 14:52:57 -0500

    Added center rectangle and surrounding triangles

--- stainedGlass.js
+++ stainedGlass.js
@@ -41,6 +41,88 @@ scene.add(stainedGlassFrameMesh);
 
 
 // Shapes
+// Center rectangle
+var rectangleCenter = new THREE.Shape();
+
+rectangleCenter.moveTo(-150, 30);
+rectangleCenter.lineTo(-150, -30);
+rectangleCenter.lineTo(150, -30);
+rectangleCenter.lineTo(150, 30);
+rectangleCenter.lineTo(-150, 30);
+
+var rectangleCenterGeometry = new THREE.ShapeGeometry(rectangleCenter);
+var rectangleCenterMaterial = new THREE.MeshBasicMaterial({color: 0xbb0000});
+var rectangleCenterMesh =
+	new THREE.Mesh(rectangleCenterGeometry, rectangleCenterMaterial);
+
+scene.add(rectangleCenterMesh);
+
+// North pointy triangle
+var trianglePointyNorth = new THREE.Shape();
+
+trianglePointyNorth.moveTo(0, 200);
+trianglePointyNorth.lineTo(25, 50);
+trianglePointyNorth.lineTo(-25, 50);
+trianglePointyNorth.lineTo(0, 200);
+
+var trianglePointyNorthGeometry = new THREE.ShapeGeometry(trianglePointyNorth);
+var trianglePointyNorthMaterial =
+	new THREE.MeshBasicMaterial({color: 0xaaf000});
+var trianglePointyNorthMesh =
+	new THREE.Mesh(trianglePointyNorthGeometry, trianglePointyNorthMaterial);
+
+scene.add(trianglePointyNorthMesh);
+
+// West pointy triangle
+var trianglePointyWest = new THREE.Shape();
+
+trianglePointyWest.moveTo(-320, 0);
+trianglePointyWest.lineTo(-170, -25);
+trianglePointyWest.lineTo(-170, 25);
+trianglePointyWest.lineTo(-320, 0);
+
+var trianglePointyWestGeometry = new THREE.ShapeGeometry(trianglePointyWest);
+var trianglePointyWestMaterial =
+	new THREE.MeshBasicMaterial({color: 0xaaf000});
+var trianglePointyWestMesh =
+	new THREE.Mesh(trianglePointyWestGeometry, trianglePointyWestMaterial);
+
+scene.add(trianglePointyWestMesh);
+
+// South pointy triangle
+var trianglePointySouth = new THREE.Shape();
+
+trianglePointySouth.moveTo(0, -200);
+trianglePointySouth.lineTo(-25, -50);
+trianglePointySouth.lineTo(25, -50);
+trianglePointySouth.lineTo(0, -200);
+
+var trianglePointySouthGeometry = new THREE.ShapeGeometry(trianglePointySouth);
+var trianglePointySouthMaterial =
+	new THREE.MeshBasicMaterial({color: 0xaaf000});
+var trianglePointySouthMesh =
+	new THREE.Mesh(trianglePointySouthGeometry, trianglePointySouthMaterial);
+
+scene.add(trianglePointySouthMesh);
+
+// East pointy triangle
+var trianglePointyEast = new THREE.Shape();
+
+trianglePointyEast.moveTo(320, 0);
+trianglePointyEast.lineTo(170, 25);
+trianglePointyEast.lineTo(170, -25);
+trianglePointyEast.lineTo(320, 0);
+
+var trianglePointyEastGeometry = new THREE.ShapeGeometry(trianglePointyEast);
+var trianglePointyEastMaterial =
+	new THREE.MeshBasicMaterial({color: 0xaaf000});
+var trianglePointyEastMesh =
+	new THREE.Mesh(trianglePointyEastGeometry, trianglePointyEastMaterial);
+
+scene.add(trianglePointyEastMesh);
+
+// Northwest horn
+// TODO 
 
 
 // Render
